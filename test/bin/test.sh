69db935e8dcefdbe3a78dee4bcd022d33f34ce14
Sonja Heinze <sonjaleaheinze@gmail.com>
2022-04-14 19:50:52 +0200

    Improve the testing workflow
    Co-authored-by: panglesd <panglesd@users.noreply.github.com>

--- test/bin/test.sh
+++ test/bin/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-docker build -t test test/bin
-docker run --rm -it -v $(pwd)/_build/default/bin:/root/platform --privileged test
+docker build -t test .
+# docker run --rm -it --privileged test
 # docker run --rm -i -v $(pwd)/_build/default/bin:/root/platform --privileged test <<EOF
 # ./root/platform/main.exe
 # opam init -y
