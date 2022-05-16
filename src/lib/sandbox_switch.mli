3b0fc38ba02dadb46c9df3304cde65a5085dbd46
Paul-Elliot <peada@free.fr>
2022-05-16 12:46:01 +0200

    remove sandbox switch after getting the binaries
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/lib/sandbox_switch.mli
+++ src/lib/sandbox_switch.mli
@@ -8,3 +8,8 @@ val pin : t -> pkg:string -> url:string -> (unit, [> R.msg ]) result
 val install : t -> pkgs:string list -> (unit, [> R.msg ]) result
 val list_files : t -> pkg:string -> (Fpath.t list, [> R.msg ]) result
 val switch_path_prefix : t -> Fpath.t
+
+val with_sandbox_switch :
+  ocaml_version:Ocaml_version.t ->
+  (t -> ('a, ([> R.msg ] as 'b)) result) ->
+  ('a, 'b) result
