9b52cce56087c4c680ddb0471dc5123ba04fbf32
Paul-Elliot <peada@free.fr>
2022-09-15 09:42:43 +0200

    fix version for `ocaml-system` sandbox pkg, keep original `ocaml`
    `ocaml-system` needs to have the same version as `Sys.ocaml_version`. The
    documentation says that this value will keep the `+` and `~` suffixes, but
    experiments show that for the package `ocaml-variants.4.14.0~rc2+options`, the
    value of `Sys.ocaml_version` is `4.14.0~rc2`. So we prefer to disable the
    version check.
    With the test disabled, we are no longer refrained from using the same package
    for all suffixed version, by unsuffixing the version of `ocaml-system`. This
    also allows us to not patch the `ocaml` package.
    
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/lib/sandbox_compiler_package.ml
+++ src/lib/sandbox_compiler_package.ml
@@ -21,7 +21,7 @@ conflict-class: "ocaml-core-compiler"
 flags: compiler
 build: ["ocaml" "gen_ocaml_config.ml"]
 substs: "gen_ocaml_config.ml"
-extra-files: ["gen_ocaml_config.ml.in" "md5=093e7bec1ec95f9e4c6a313d73c5d840"]
+extra-files: ["gen_ocaml_config.ml.in" "md5=c2a459eda2f5a95e67cd4114447b8a1b"]
 |}
 
 let extra_file =
@@ -35,15 +35,6 @@ let extra_file =
       else
         (s, "") in
     base ^ "c" ^ suffix in
-  if Sys.ocaml_version <> "%{_:version}%" then
-    (Printf.eprintf
-       "ERROR: The compiler found at %%s has version %%s,\n\
-        and this package requires %{_:version}%.\n\
-        You should use e.g. 'opam switch create %{_:name}%.%%s' \
-        instead."
-       ocamlc Sys.ocaml_version Sys.ocaml_version;
-     exit 1)
-  else
   let ocamlc_digest = Digest.to_hex (Digest.file ocamlc) in
   let libdir =
     if Sys.command (ocamlc^" -where > %{_:name}%.config") = 0 then
@@ -70,48 +61,11 @@ let extra_file =
   close_out oc
 |}
 
-(** The [~alpha...] suffix needs to be removed when overriding the [ocaml]
+(** The [~alpha...+...] suffix needs to be removed when overriding the [ocaml]
     package. *)
-let remove_alpha_suffix ver =
-  match String.cut ~sep:"~" ver with Some (pre, _) -> pre | None -> ver
-
-(** Lookup in the selected switch. *)
-let lookup_ocaml_descr opam_opts = Opam.Show.opam_file opam_opts ~pkg:"ocaml"
-
-(** Fix [ocaml]'s constraint on [ocaml-system], which disallow alpha versions. *)
-let fix_ocaml_constraints opam_file ~ocaml_version =
-  let open OpamParserTypes.FullPos in
-  let with_pos f wp = { wp with pelem = f wp.pelem } in
-  let fake_pos pelem =
-    { pelem; pos = { filename = ""; start = (0, 0); stop = (0, 0) } }
-  in
-  let name_is_ocaml t =
-    match t.pelem with String "ocaml-system" -> true | _ -> false
-  in
-  let rec fix_depend = function
-    | Option (name, _args) when name_is_ocaml name ->
-        let cnstr =
-          Prefix_relop (fake_pos `Eq, fake_pos (String ocaml_version))
-        in
-        Option (name, fake_pos [ fake_pos cnstr ])
-    | Logop (op, left, right) ->
-        Logop (op, with_pos fix_depend left, with_pos fix_depend right)
-    | t -> t
-  in
-  let fix_depends = function
-    | List elems -> List (with_pos (List.map (with_pos fix_depend)) elems)
-    | t -> t
-  in
-  let fix_item = function
-    | Variable (({ pelem = "depends"; _ } as name), value) ->
-        Variable (name, with_pos fix_depends value)
-    | t -> t
-  in
-  let fix_opamfile t =
-    { t with file_contents = List.map (with_pos fix_item) t.file_contents }
-  in
-  OpamParser.FullPos.string opam_file "ocaml"
-  |> fix_opamfile |> OpamPrinter.FullPos.opamfile
+let remove_alpha_plus_suffix ver =
+  let cut sep s = match String.cut ~sep s with Some (s, _) -> s | None -> s in
+  cut "+" (cut "~" ver)
 
 (** Override the [ocaml-system] package to drop the [sys-ocaml-version]
     constraint and to specify the right version for it, for example
@@ -127,23 +81,12 @@ let init_pkg_ocaml_system repo ~ocaml_version =
     let extra_files = [ ("gen_ocaml_config.ml.in", extra_file) ] in
     Repo.add_package repo pkg ~extra_files ~install_file opam_file
 
-(** Override the [ocaml] package to fix the dependency it has on [ocaml-system],
-    which disallow [~] suffixes. The [ocaml] package itself must not use a [~]
-    suffix. *)
-let init_pkg_ocaml opam_opts repo ~ocaml_version =
-  let pkg = Package.v ~name:"ocaml" ~ver:(remove_alpha_suffix ocaml_version) in
-  if Repo.has_pkg repo pkg then Ok ()
-  else
-    let* opam_file = lookup_ocaml_descr opam_opts in
-    let opam_file = fix_ocaml_constraints opam_file ~ocaml_version in
-    Repo.add_package repo pkg (Package.Opam_file.of_string opam_file)
-
 let init opam_opts ocaml_version =
+  let ocaml_version = remove_alpha_plus_suffix ocaml_version in
   let name = "platform_sandbox_compiler_packages" in
   let path =
     Fpath.(opam_opts.Opam.GlobalOpts.root / "plugins" / "ocaml-platform" / name)
   in
   let* repo = Repo.init ~name path in
   let* () = init_pkg_ocaml_system repo ~ocaml_version in
-  let* () = init_pkg_ocaml opam_opts repo ~ocaml_version in
-  Ok repo
+  Ok (repo, "ocaml-system."^ocaml_version)
