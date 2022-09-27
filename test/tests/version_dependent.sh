d76adaf04612084e970dd93e72df3e9ed3951e20
Paul-Elliot <peada@free.fr>
2022-09-27 17:37:53 +0200

    Add a test for ocaml version dependent/independent binary packages
    Signed-off-by: Paul-Elliot <peada@free.fr>

---
+++ test/tests/version_dependent.sh
@@ -0,0 +1,32 @@
+#/usr/bin/env bash
+set -euo pipefail
+
+# The setting of this test should be the end of the "install" test. That is, the
+# current switch should be of version 4.08 with the installer having run.
+
+# The installed version of these tools should be tied to 4.08.1, as they are
+# dependent on the version of OCaml they were compiled with.
+[[ $(opam show dune -f depends: ) =~ "4.08.1" ]]
+[[ $(opam show merlin+bin+platform -f depends: ) =~ "4.08.1" ]]
+[[ $(opam show ocaml-lsp-server+bin+platform -f depends: ) =~ "4.08.1" ]]
+[[ $(opam show odoc+bin+platform -f depends: ) =~ "4.08.1" ]]
+
+# The installed version of these tools should not be tied to 4.08.1, as they are
+# is independent from the version of OCaml they were compiled with.
+[[ $(opam show dune-release+bin+platform -f depends: ) != *"4.08.1"* ]]
+[[ $(opam show ocamlformat+bin+platform -f depends: ) != *"4.08.1"* ]]
+
+# Now, we check that the installation still works well on 4.13
+
+opam switch create 4.13.1
+
+eval $(opam env)
+
+ocaml-platform
+
+opam --version
+dune --version
+dune-release --version
+dune ocaml-merlin --version
+odoc --version
+ocamlformat --version
