60b0c9641c0796ed82bf3c333ba0981d5895e8e5
Paul-Elliot <peada@free.fr>
2022-09-29 16:14:27 +0200

    fix cache emptyness check
    The emptyness check was checking that the cache was not created, we want to
    check that it is empty, eg no packages were added.
    
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- test/tests/check_cache_is_empty.sh
+++ test/tests/check_cache_is_empty.sh
@@ -1,4 +1,9 @@
 #/usr/bin/env bash
 set -euo pipefail
 
-[[ $(find $HOME/.opam/plugins/ocaml-platform/cache/archives) = "" ]]
+[[ $(find $HOME/.opam/plugins/ocaml-platform/cache/archives/odoc+bin+platform) = "" ]]
+[[ $(find $HOME/.opam/plugins/ocaml-platform/cache/archives/dune) = "" ]]
+[[ $(find $HOME/.opam/plugins/ocaml-platform/cache/archives/merlin+bin+platform) = "" ]]
+[[ $(find $HOME/.opam/plugins/ocaml-platform/cache/archives/ocaml-lsp-server+bin+platform) = "" ]]
+[[ $(find $HOME/.opam/plugins/ocaml-platform/cache/archives/ocamlformat+bin+platform) = "" ]]
+[[ $(find $HOME/.opam/plugins/ocaml-platform/cache/archives/dune-release+bin+platform) = "" ]]
