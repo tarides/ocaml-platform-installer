10dbeb6a9199735761e17228ffe9fb8af7e2a019
Paul-Elliot <peada@free.fr>
2022-12-20 16:51:19 +0100

    Fix CI failing due to new OCaml release
    At the date of this commit, a new version of OCaml 4.14 was released. It might
    be the cause of the CI failing (maybe because docker images were not updated?).
    Testing on a pinned compiler version 4.12 fixes this.
    
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- test/tests/pin-ocaml-compiler.sh
+++ test/tests/pin-ocaml-compiler.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 cd $HOME
-git clone --branch 4.14 --single-branch https://github.com/ocaml/ocaml
+git clone --branch 4.12 --single-branch https://github.com/ocaml/ocaml
 cd ocaml
 opam switch create --empty pinned
 opam install .
